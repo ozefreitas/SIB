36e1c7726f9b78b5217ec1531f8520f7445e8be3
pg42872 <pg42872@alunos.uminho.pt>
2021-11-02 14:27:14 +0000

    debug

--- tests/test_1.py
+++ tests/test_1.py
@@ -4,7 +4,7 @@ import os
 
 
 try:
-    import si
+    import src.si
 except:
     DIR = os.path.dirname(os.path.realpath(__file__))
     PATH = os.path.join(DIR, '../src/')
@@ -14,7 +14,7 @@ except:
 class TestUnlabeledDataset(unittest.TestCase):
 
     def setUp(self):
-        from si.data import Dataset
+        from src.si.data import Dataset
         self.filename = "datasets/lr-example1.data"
         self.dataset = Dataset.from_data(self.filename, labeled=False)
 
@@ -25,6 +25,6 @@ class TestUnlabeledDataset(unittest.TestCase):
 class TestLabeledDataset(TestUnlabeledDataset):
 
     def setUp(self):
-        from si.data import Dataset
+        from src.si.data import Dataset
         self.filename = "datasets/lr-example1.data"
         self.dataset = Dataset.from_data(self.filename, labeled=True)
