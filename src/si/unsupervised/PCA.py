70cdfd215a4cba43febacb9734afdd5fb00bc88e
pg42872 <pg42872@alunos.uminho.pt>
2021-11-03 15:18:17 +0000

    Updating

--- src/si/unsupervised/PCA.py
+++ src/si/unsupervised/PCA.py
@@ -6,21 +6,21 @@ class PCA:
     def __init__(self, num_components=2):
         self.numcomps = num_components
 
-    def fit(self, dataset):  # objeto Dataset
-        pass
+#    def fit(self, dataset):  # objeto Dataset
+#        pass
 
     def transform(self, dataset):  # objeto Dataset
         x_scaled = StandardScaler.fit_transform(dataset)  # standardização dos dados usando o StandardScaler
 
         matriz_cov = np.cov(x_scaled, rowvar=False)  #
 
-        eigen_values, eigen_vectors = np.linalg.eigh(matriz_cov)
+        self.eigen_values, self.eigen_vectors = np.linalg.eigh(matriz_cov)
 
         # sort the eigenvalues in descending order
-        sorted_index = np.argsort(eigen_values)[::-1]  # np.argsort returns an array of indices of the same shape.
-        sorted_eigenvalue = eigen_values[sorted_index]
+        self.sorted_index = np.argsort(self.eigen_values)[::-1]  # np.argsort returns an array of indices of the same shape.
+        self.sorted_eigenvalue = self.eigen_values[self.sorted_index]
         # similarly sort the eigenvectors
-        sorted_eigenvectors = eigen_vectors[:, sorted_index]
+        sorted_eigenvectors = self.eigen_vectors[:, self.sorted_index]
 
         # select the first n eigenvectors, n is desired dimension of our final reduced data.
         # you can select any number of components.
@@ -28,3 +28,12 @@ class PCA:
 
         x_reduced = np.dot(eigenvector_subset.transpose(), x_scaled.transpose()).transpose()
         return x_reduced
+
+    def explained_variance(self, dataset):
+        self.sorted_eigenvalue_sub = self.sorted_eigenvalue[0:self.numcomps]
+        return np.sum(self.sorted_eigenvalue_sub), self.sorted_eigenvalue
+
+    def fit_transform(self, dataset):
+        data_reduced = self.transform(dataset)
+        explain, eigvalues = self.explained_variance(dataset)
+        return data_reduced, explain, eigvalues
