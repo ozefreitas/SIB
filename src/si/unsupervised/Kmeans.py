be57fd9de4c32e189f1414f478508c71196e4dfa
pg42872 <pg42872@alunos.uminho.pt>
2021-11-08 10:44:33 +0000

    Bug fixes

--- src/si/unsupervised/Kmeans.py
+++ src/si/unsupervised/Kmeans.py
@@ -17,7 +17,8 @@ class KMeans:
         self.k = k
         self.n = max_iterations
         self.centroides = None
-        self.measure = measure
+        if measure is "euclidean":
+            self.measure = euclidean
 
     #   def distance_12(self, x, y):
     #       """
@@ -33,36 +34,29 @@ class KMeans:
         self.min = np.min(dataset.X, axis=0)  # fazer a média em relação às features
         self.max = np.max(dataset.X, axis=0)
 
-    def init_centroids(self, dataset):
+    def init_centroides(self, dataset):
         x = dataset.X
-        centroides = []
-        for i in range(x.shape[1]):
-            centroides.append(np.random.uniform(low=self.min[i], high=self.max[i], size=(self.k,)))
-        self.centroides = np.array(centroides).transpose()
+        self.centroides = np.array([np.random.uniform(low=self.min[i], high=self.max[i], size=(self.k,))
+                                   for i in range(x.shape[1])]).T
 
     def get_closest_centroid(self, x):
-        if self.measure is "euclidean":
-            dist = euclidean(x, self.centroides)
-            closest_centroid_index = np.argmin(dist, axis=0)
-        else:
-            dist = manhattan(x, self.centroides)
-            closest_centroid_index = np.argmin(dist, axis=0)
+        dist = self.measure(x, self.centroides)
+        closest_centroid_index = np.argmin(dist, axis=0)
         return closest_centroid_index
 
     def transform(self, dataset):
-        self.init_centroids(dataset)
+        self.init_centroides(dataset)
         x = dataset.X
-        changed = True
+        changed = False
         count = 0
         old_idxs = np.zeros(x.shape[0])
-        while changed is True or count < self.n:
-            idxs = np.apply_along_axis(self.get_closest_centroid, axis=0, arr=x.transpose())
-            cent = [np.mean(x[idxs == i]) for i in range(x.shape[0])]
-            self.centroides = np.array(cent)
+        while count < self.n and not changed:
+            idxs = np.apply_along_axis(self.get_closest_centroid, axis=0, arr=x.T)
+            self.centroids = np.array([np.mean(x[idxs == i], axis=0) for i in range(self.k)])
             changed = np.all(old_idxs == idxs)
             old_idxs = idxs
             count += 1
-        return self.centroides, old_idxs
+        return self.centroids, old_idxs
 
     def fit_transform(self, dataset):
         self.fit(dataset)
